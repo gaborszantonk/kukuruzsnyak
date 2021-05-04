149a7a19487394efc4d5196ed26c0d62b5f5435c
Pisti15 <you@example.com>
2021-05-04 21:34:42 +0200

    valtozas

--- maxmin.py
+++
@@ -1,9 +0,0 @@
-print("Ez a program számokat kér be, majd megmondja, \n melyik volt a legnagyobb és a legkisebb szám")
-lista = []
-a = int(input("Hányszor fogsz megadni számokat?: "))
-for i in range(1, 6):
-    ele = int(input())
-    lista.append(ele)
-print(lista)
-print (max(lista ))
-print (min(lista ))
